edefd31a3ccc37d07b32b2f5a99fff6a960f4529
Cybernetics <mail@cybernetics.me>
2025-07-04 10:45:42 +0530

    smal updates

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Exception;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\RedirectResponse;
@@ -14,14 +15,17 @@ class VerifyEmailController extends Controller
      */
     public function __invoke(EmailVerificationRequest $request): RedirectResponse
     {
-        if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended(route('dashboard', absolute: false) . '?verified=1');
-        }
+        try {
+            if ($request->user()->hasVerifiedEmail()) {
+                return redirect()->intended(route('dashboard', absolute: false) . '?verified=1');
+            }
 
-        if ($request->user()->markEmailAsVerified()) {
-            event(new Verified($request->user()));
+            if ($request->user()->markEmailAsVerified()) {
+                event(new Verified($request->user()));
+            }
+        } catch (Exception $e) {
+            return redirect()->route('verify.email')->with("error", "You should have logged in first to verify your email id.");
         }
-
         return redirect()->intended(route('dashboard', absolute: false) . '?verified=1')->with("success", "Email has verified successfully!");
     }
 }
